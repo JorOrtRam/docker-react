31b13ce6906a1a6d0fec5de429aa54f896975111
Jorge Ortiz <jortizr@minsait.com>
2022-07-20 10:59:16 -0500

    Second chance AWS Beanstalk

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          AWS confuses me!
+          AWS not working!
         </p>
         <a
           className="App-link"
